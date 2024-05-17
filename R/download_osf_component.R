eb03b697343d8163da1b86af2517df7b2e78f706
Christoph Adrian <christoph.adrian@fau.de>
2024-05-17 14:37:24 +0200

    Simplify function download_osf_component

--- R/download_osf_component.R
+++ R/download_osf_component.R
@@ -1,34 +1,31 @@
 #' Download data folder from OSF
 #'
-#' This function is design to download all files from (at least) one OSF component.
+#' This function is design to simplify and help download files from OSF, especially from multiple OSF components (based on their ID) at once.
 #'
 #' @param local_folder A character vector specifying the folder(s) to save the downloaded data into. Will be used to add to argument 'local_directory'.
 #' @param local_directory The directory where data will be downloaded. Default is "local_data/osf_pull".
-#' @param osf_component A character vector specifying the corresponding node ID(s) for each component.
-#' @param osf_node An OSF project or (super-)component ID, which is the parent of the component(s) to download.
+#' @param osf_component_id  A character vector specifying the corresponding node ID(s) for each component.
 #'
 #' @return No explicit return value. Data will be downloaded to the specified directory.
-#' @importFrom osfr osf_download osf_ls_nodes osf_ls_files
+#' @importFrom osfr osf_download osf_retrieve_node osf_ls_nodes osf_ls_files
 #' @importFrom here here
 #' @export
 #' @examples
 #' \dontrun{
 #' local_folder <- c("base")
-#' osf_component <- c("Base data")
-#' osf_node <- osfr::osf_retrieve_node("Your OSF project/component")
-#' download_osf_data(local_folder, osf_component, osf_node)
+#' osf_component <- c("jmipd")
+#' download_osf_data(local_folder, osf_component)
 #' }
 
 download_osf_component <- function(
     local_folder,
-    osf_component,
-    osf_node,
+    osf_component_id,
     # optional
     local_directory = "local_data/osf_pull") {
 
   for (i in seq_along(local_folder)) {
     download_folder <- local_folder[i]
-    osf_node_component <- osf_component[i]
+    osf_node_component <- osf_component_id[i]
 
     # Check if directory exists
     download_dir_folder <- here::here(local_directory, download_folder)
@@ -38,9 +35,7 @@ download_osf_component <- function(
 
     # Download data
     osfr::osf_download(
-      osf_node |>
-        osfr::osf_ls_nodes(osf_node_component) |>
-        osfr::osf_ls_files(n_max = 100),
+      osfr::osf_retrieve_node(osf_node_component) |> osfr::osf_ls_files(n_max = 100),
       path = download_dir_folder,
       recurse = TRUE,
       conflicts = "overwrite",
--- tests/testthat/test-download_osf_component.R
+++ tests/testthat/test-download_osf_component.R
@@ -1,6 +1,6 @@
 #' Download data folder from OSF
 #'
-#' This function is design to download all files from (at least) one OSF component.
+#' This function is design to simplify and help download files from OSF, especially from multiple OSF components (based on their ID) at once.
 #'
 #' @param local_folder A character vector specifying the folder(s) to save the downloaded data into. Will be used to add to argument 'local_directory'.
 #' @param local_directory The directory where data will be downloaded. Default is "local_data/osf_pull".
@@ -8,7 +8,7 @@
 #' @param osf_node An OSF project or (super-)component ID, which is the parent of the component(s) to download.
 #'
 #' @return No explicit return value. Data will be downloaded to the specified directory.
-#' @importFrom osfr osf_download osf_ls_nodes osf_ls_files
+#' @importFrom osfr osf_download osf_retrieve_node osf_ls_nodes osf_ls_files
 #' @importFrom here here
 #' @export
 #' @examples
@@ -21,14 +21,13 @@
 
 download_osf_component <- function(
     local_folder,
-    osf_component,
-    osf_node,
+    osf_component_id,
     # optional
     local_directory = "local_data/osf_pull") {
 
   for (i in seq_along(local_folder)) {
     download_folder <- local_folder[i]
-    osf_node_component <- osf_component[i]
+    osf_node_component <- osf_component_id[i]
 
     # Check if directory exists
     download_dir_folder <- here::here(local_directory, download_folder)
@@ -38,9 +37,7 @@ download_osf_component <- function(
 
     # Download data
     osfr::osf_download(
-      osf_node |>
-        osfr::osf_ls_nodes(osf_node_component) |>
-        osfr::osf_ls_files(n_max = 100),
+      osfr::osf_retrieve_node(osf_node_component) |> osfr::osf_ls_files(n_max = 100),
       path = download_dir_folder,
       recurse = TRUE,
       conflicts = "overwrite",
