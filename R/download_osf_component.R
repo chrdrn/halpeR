8b682b1819d1a34c875b7fb24561290746d279a8
Christoph Adrian <christoph.adrian@fau.de>
2025-02-15 10:12:25 +0100

    Update function "download_osf_component.R"

--- R/download_osf_component.R
+++ R/download_osf_component.R
@@ -1,45 +1,52 @@
-#' Download data folder from OSF
+
+#' Download OSF Component
 #'
-#' This function is design to simplify and help download files from OSF, especially from multiple OSF components (based on their ID) at once.
+#' This function downloads components from the Open Science Framework (OSF) to a specified local directory.
 #'
-#' @param local_folder A character vector specifying the folder(s) to save the downloaded data into. Will be used to add to argument 'local_directory'.
-#' @param local_directory The directory where data will be downloaded. Default is "local_data/osf_pull".
-#' @param osf_component_id  A character vector specifying the corresponding node ID(s) for each component.
+#' @param local_folder A character vector of local folder paths where the OSF components will be downloaded.
+#' @param osf_component_id A character vector of OSF component IDs corresponding to the local folders.
+#' @param local_directory A character string specifying the base local directory for the OSF pull. Default is "local_data/osf_pull".
 #'
-#' @return No explicit return value. Data will be downloaded to the specified directory.
-#' @importFrom osfr osf_download osf_retrieve_node osf_ls_nodes osf_ls_files
-#' @importFrom here here
-#' @export
+#' @details
+#' The function uses `purrr::walk2` to iterate over the `local_folder` and `osf_component_id` vectors, downloading each component to the corresponding local folder.
+#' If the specified local folder does not exist, it will be created. The function handles errors gracefully, printing a message if a download fails.
+#'
+#' @importFrom purrr walk2
+#' @importFrom osfr osf_retrieve_node osf_ls_files osf_download
+#'
 #' @examples
 #' \dontrun{
-#' local_folder <- c("base")
-#' osf_component <- c("jmipd")
-#' download_osf_data(local_folder, osf_component)
+#' local_folders <- c("data/project1", "data/project2")
+#' osf_component_ids <- c("abc123", "def456")
+#' download_osf_component(local_folders, osf_component_ids)
 #' }
+#'
+#' @export
 
-download_osf_component <- function(
-    local_folder,
-    osf_component_id,
-    # optional
-    local_directory = "local_data/osf_pull") {
-
-  for (i in seq_along(local_folder)) {
-    download_folder <- local_folder[i]
-    osf_node_component <- osf_component_id[i]
-
-    # Check if directory exists
-    download_dir_folder <- here::here(local_directory, download_folder)
-    if (!dir.exists(download_dir_folder)) {
-      dir.create(download_dir_folder, recursive = TRUE)
+download_osf_component <- function(local_folder,
+                                   osf_component_id,
+                                   local_directory = "local_data/osf_pull") {
+  purrr::walk2(local_folder, osf_component_id, function(folder, component_id) {
+    download_component <- function(download_dir_folder, component_id) {
+      tryCatch({
+        # Check if directory exists
+        if (!dir.exists(download_dir_folder)) {
+          dir.create(download_dir_folder, recursive = TRUE)
+        }
+        node <- osfr::osf_retrieve_node(component_id)
+        # Download data
+        files <- osfr::osf_ls_files(node)
+        osfr::osf_download(
+          files,
+          path = download_dir_folder,
+          recurse = TRUE,
+          conflicts = "overwrite"
+        )
+      }, error = function(e) {
+        message(sprintf("Error downloading %s: %s", component_id, e$message))
+      })
     }
 
-    # Download data
-    osfr::osf_download(
-      osfr::osf_retrieve_node(osf_node_component) |> osfr::osf_ls_files(n_max = 100),
-      path = download_dir_folder,
-      recurse = TRUE,
-      conflicts = "overwrite",
-      verbose = TRUE
-    )
-  }
+    download_component(folder, component_id)
+  })
 }
